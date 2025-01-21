a629c42bba395dc4f64c7de22e1a05ec626e3bda
gurpinder009 <gurpinders1511@gmail.com>
2025-01-21 12:02:56 -0800

    classwork2 done

--- app/src/main/java/com/example/classwork/MainActivity.kt
+++ app/src/main/java/com/example/classwork/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.classwork
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
@@ -8,40 +9,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.classwork.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
 
-
-
-
-
-
+    @SuppressLint("SetTextI18n")
     fun convertCurrency(view: View){
-        val dollarInput = findViewById<EditText>(R.id.dollar_input)
-        val outputText = findViewById<TextView>(R.id.output_txt)
-
-        if(dollarInput.text.isNotEmpty()){
-            val dollarValue:Float = dollarInput.text.toString().toFloat();
-            outputText.text = (dollarValue * 0.85f).toString();
+        if(binding.dollarInput.text.isNotEmpty()){
+            val dollarValue:Float = binding.dollarInput.text.toString().toFloat();
+            binding.outputTxt.text = (dollarValue * 0.85f).toString();
         }else{
-            outputText.text = getString(R.string.no_value_msg)
+            binding.outputTxt.text = getString(R.string.no_value_msg)
         }
-
-
-
-
     }
-
-
-
 }
